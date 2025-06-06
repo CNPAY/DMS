2e40a1340339b07e305310f8536e5eca70b610fa
Jakey <6077782@qq.com>
2025-06-07 00:33:54 +0800

    [FIX]环境变量初始化数据库配置

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -9,13 +9,15 @@ declare global {
 }
 
 // 创建或获取全局Prisma实例
-export const prisma = globalThis.__prisma || new PrismaClient({
-  datasources: {
-    db: {
-      url: dbConfig.mysql.url
-    }
-  }
-})
+// export const prisma = globalThis.__prisma || new PrismaClient({
+//   datasources: {
+//     db: {
+//       url: dbConfig.mysql.url
+//     }
+//   }
+// })
+export  const prisma = new PrismaClient()
+
 
 // 在开发环境中保存到全局变量，避免热重载时重复创建连接
 if (process.env.NODE_ENV !== 'production') {
