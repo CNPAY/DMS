e88b9a73ab8d4f34fd9f158c7504fd1e8a6f656e
Jakey <6077782@qq.com>
2025-06-11 14:42:00 +0800

    [FIX]移除无用图片

--- store/modules/user.ts
+++ store/modules/user.ts
@@ -1,6 +1,6 @@
 // import { login, logout, getInfo, loginOuth } from '@/api/login';
 import { getToken, setToken, removeToken } from '@/utils/auth';
-import defAva from '@/assets/images/profile.jpg';
+import defAva from '';
 import { defineStore } from 'pinia';
 
 // 用户相关的类型定义
